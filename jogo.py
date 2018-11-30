d2428e069f076ad3996d56c0d6404c159e513e6b
Gabriel de Almeida Guerreiro <gabrieldag96@gmail.com>
2018-11-30 01:24:46 -0200

    feito a remoção e colocado no mapa o item

--- jogo.py
+++ jogo.py
@@ -183,27 +183,32 @@ def jogo():
                         primeiros_socorros_grande = Sprite("imagens/primeiros-socorros-grande.png")
                         primeiros_socorros_grande.set_position(20, primeiros_socorros_grande.height + y )
                         bolsa.append(primeiros_socorros_grande)
+                        bolsa_nome.append("primeiros_socorros")
                     if(itens_nome[posicao] == "seringa"):
                         seringa_grande = Sprite("imagens/seringa-grande.png")
                         seringa_grande.set_position(20, seringa_grande.height + y )
                         bolsa.append(seringa_grande)
+                        bolsa_nome.append("seringa")
                     if(itens_nome[posicao] == "chave"):
                         chave_grande = Sprite("imagens/chave-grande.png")
                         chave_grande.set_position(20, chave_grande.height + y )
                         bolsa.append(chave_grande)
+                        bolsa_nome.append("chave")
                     if(itens_nome[posicao] == "granada"):
                         granada_grande = Sprite("imagens/granada-grande.png")
                         granada_grande.set_position(20, granada_grande.height + y)
                         bolsa.append(granada_grande)
+                        bolsa_nome.append("granada")
                     if(itens_nome[posicao] == "dinamite"):
                         dinamite_grande = Sprite("imagens/dinamite-grande.png")
                         dinamite_grande.set_position(20, dinamite_grande.height + y)
+                        bolsa_nome.append("dinamite")
                         bolsa.append(dinamite_grande)
-                    bolsa_nome.append(itens_nome[posicao])
                     itens.remove(i)
                     itens_nome.remove(itens_nome[posicao])
                     print(bolsa)
                     print(bolsa_nome)
+                    pygame.time.wait(150)
                     y = y + 130
                     #itens.bolsa_draw(bolsa)
         
@@ -211,10 +216,37 @@ def jogo():
         if teclado.key_pressed('z'):
             print(y)
             if len(bolsa)>=1:
+                if bolsa_nome[-1] == "seringa":
+                    seringa = Sprite("imagens/seringa.png")
+                    seringa.set_position(joy_play.x, joy_play.y+joy_play.height-seringa.height)
+                    itens.append(seringa)
+                    itens_nome.append("seringa")
+                if bolsa_nome[-1] == "dinamite":
+                    dinamite = Sprite("imagens/dinamite.png")
+                    dinamite.set_position(joy_play.x, joy_play.y+joy_play.height-dinamite.height)
+                    itens.append(dinamite)
+                    itens_nome.append("dinamite")
+                if bolsa_nome[-1] == "primeiros_socorros":
+                    primeirossocorros = Sprite("imagens/primeiros-socorros.png")
+                    primeirossocorros.set_position(joy_play.x, joy_play.y+joy_play.height-primeirossocorros.height)
+                    itens.append(primeirossocorros)
+                    itens_nome.append("primeiros_socorros")
+                if bolsa_nome[-1] == "chave":
+                    chave = Sprite("imagens/chave.png")
+                    chave.set_position(joy_play.x, joy_play.y+joy_play.height-chave.height)
+                    itens.append(chave)
+                    itens_nome.append("chave")
+                if bolsa_nome[-1] == "granada":
+                    granada = Sprite("imagens/granada.png")
+                    granada.set_position(joy_play.x, joy_play.y+joy_play.height-granada.height)
+                    itens.append(granada)
+                    itens_nome.append("granada")
                 bolsa.remove(bolsa[-1])
                 bolsa_nome.remove(bolsa_nome[-1])
                 y = y - 130
                 pygame.time.wait(150)
+                print(itens)
+                #print(len(itens))
         #print(len(bolsa))
         #itens.bolsa_draw(bolsa)  
         for i in range(len(bolsa)):
